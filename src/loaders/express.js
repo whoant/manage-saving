8fb9b0f1a7cee2dd4bcb5bc5e7c000b57910c78b
Vo Van Hoang Tuan <vovanhoangtuan4.2@gmail.com>
2022-01-01 10:46:56 +0700

    Test

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const methodOverride = require('method-override');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const {flash} = require('express-flash-message');
+const express = require("express");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const { flash } = require("express-flash-message");
 
-const path = require('path');
+const path = require("path");
 
-const route = require('../routes');
-const {SESSION_SECRET} = require('../config');
+const route = require("../routes");
+const { SESSION_SECRET } = require("../config");
 
 module.exports = (app) => {
-    app.use(express.urlencoded({extended: true}));
-    app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-    app.use(methodOverride('_method'));
-    app.set('view engine', 'pug');
-    app.set('views', path.join(__dirname, '../', 'views'));
+  app.use(methodOverride("_method"));
+  app.set("view engine", "pug");
+  app.set("views", path.join(__dirname, "../", "views"));
 
-    app.use(express.static(path.join(__dirname, '../', 'public')));
-    app.use('/css', express.static(path.join(__dirname, '../', 'public', 'css')));
-    app.use(cookieParser(SESSION_SECRET));
-    app.use(
-        session({
-            secret: SESSION_SECRET,
-            resave: false,
-            saveUninitialized: true,
-            cookie: {
-                maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
-            },
-        })
-    );
-    app.use(flash({sessionKeyName: 'flashMessage'}));
+  app.use(express.static(path.join(__dirname, "../", "public")));
+  app.use("/css", express.static(path.join(__dirname, "../", "public", "css")));
+  app.use(cookieParser(SESSION_SECRET));
+  app.use(
+    session({
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+      }
+    })
+  );
+  app.use(flash({ sessionKeyName: "flashMessage" }));
 
-    route(app);
-    app.all('*', (req, res, next) => {
-        res.sendStatus(400);
-    });
+  route(app);
+  app.all("*", (req, res, next) => {
+    res.sendStatus(400);
+  });
 };
