7eee2a4673ffc9ec364ae4bd0fc66c61f64439ef
Vo Van Hoang Tuan <vovanhoangtuan4.2@gmail.com>
2022-01-01 22:55:55 +0700

    Export report daily

--- src/public/js/report.js
+++ src/public/js/report.js
@@ -1,6 +1,6 @@
 const maxYear = new Date().getFullYear();
 const monthCurrent = new Date().getMonth() + 1;
-const monthSelect = monthCurrent;
+let monthSelect = monthCurrent;
 let dayCurrent = new Date().getDate();
 
 let yearCurrent = maxYear;
@@ -21,34 +21,57 @@ for (let month = 1; month <= 12; month++) {
 }
 
 
-$(document).ready(function () {
-    $('#year').html(years.join(''));
-    $('#month').html(months.join(''));
+$(document).ready(function() {
+    $("#year").html(years.join(""));
+    $("#month").html(months.join(""));
     renderDay();
 
-    $('#year').change(function () {
+    $("#year").change(function() {
         yearCurrent = $(this).val();
         renderDay();
     });
 
-    $('#month').change(function () {
+    $("#month").change(function() {
         monthSelect = $(this).val();
         renderDay();
     });
 
-    $('#btn-confirm').click(function () {
-        const dateSelected = $('#day').val();
+    $("#btn-confirm").click(function() {
+        let dateSelected = $("#day").val();
+        if (dateSelected.length < 2) dateSelected = "0" + dateSelected;
 
+        if (monthSelect.toString().length < 2) monthSelect = "0" + monthSelect;
+
+        $("#show-day").text(`Báo cáo doanh số hoạt động ngày : ${dateSelected}/${monthSelect}/${yearCurrent}`);
+        const time = `${yearCurrent}-${monthSelect}-${dateSelected}`;
+        getReport(time);
     });
 
 });
 
 function renderDay() {
-    const totalsDay = moment(`${yearCurrent}-${monthSelect}`, "YYYY-MM").daysInMonth()
+    const totalsDay = moment(`${yearCurrent}-${monthSelect}`, "YYYY-MM").daysInMonth();
     days = Array.from(Array(totalsDay).keys()).map(day => `<option value="${day + 1}">${day + 1}</option>`);
     if (yearCurrent === maxYear && monthCurrent === monthSelect) {
         days.length = dayCurrent;
     }
     days = days.reverse();
-    $('#day').html(days.join(''));
+    $("#day").html(days.join(""));
+}
+
+async function getReport(time) {
+    try {
+        const { data } = await axios.get(`/manager/report-detail?date=${time}`);
+
+        const render = data.data.map((item, i) => renderHTML(i, item));
+        $("#tbody").empty();
+        $("#tbody").append(render.join(""));
+
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+function renderHTML(i, obj) {
+    return `<tr><th scope="row">${i + 1}</th><td>${obj.name}</td><td>${obj.in}</td><td>${obj.out}</td><td>${obj.diff}</td></tr>`;
 }
