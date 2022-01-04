e856416fcdfd65853cc179512a27e16341771675
Vo Van Hoang Tuan <vovanhoangtuan4.2@gmail.com>
2022-01-04 19:55:10 +0700

    Update format date time

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,23 +1,23 @@
-const crypto = require('crypto');
-const html_to_pdf = require('html-pdf-node');
-const pug = require('pug');
-const path = require('path');
+const crypto = require("crypto");
+const html_to_pdf = require("html-pdf-node");
+const pug = require("pug");
+const path = require("path");
 
-const compiledFunction = pug.compileFile(path.join(__dirname, '..', 'static', 'deposit.pug'));
+const compiledFunction = pug.compileFile(path.join(__dirname, "..", "static", "deposit.pug"));
 
 const hash256 = (text) => {
-    return crypto.createHash('sha256').update(text).digest('hex');
+    return crypto.createHash("sha256").update(text).digest("hex");
 };
 
-const formatDate = (time, locate = 'US') => {
+const formatDate = (time, locate = "US") => {
     const date = new Date(time);
     const year = date.getFullYear();
     let month = date.getMonth() + 1;
     let day = date.getDate();
-    if (month < 10) month = '0' + month;
-    if (day < 10) day = '0' + day;
-    if (locate === 'US') return `${year}-${month}-${day}`;
-    return `${day}-${month}-${year}`;
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+    if (locate === "US") return `${year}-${month}-${day}`;
+    return `${day}/${month}/${year}`;
 };
 
 const covertPlainObject = (objORM) => {
@@ -25,12 +25,12 @@ const covertPlainObject = (objORM) => {
 };
 
 const formatMoney = (money) => {
-    return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(money);
+    return new Intl.NumberFormat("vi-VN", { style: "currency", currency: "VND" }).format(money);
 };
 
 const randomCharacters = (length) => {
-    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let result = '';
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let result = "";
     for (let i = length; i > 0; --i) {
         result += chars[Math.floor(Math.random() * chars.length)];
     }
@@ -39,16 +39,16 @@ const randomCharacters = (length) => {
 };
 
 const generateDeposit = async ({
-    id,
-    name,
-    type,
-    createdAt,
-    expirationDate,
-    interest,
-    totalAmount,
-    typeDeposit,
-    deposit,
-}) => {
+                                   id,
+                                   name,
+                                   type,
+                                   createdAt,
+                                   expirationDate,
+                                   interest,
+                                   totalAmount,
+                                   typeDeposit,
+                                   deposit
+                               }) => {
     try {
         const content = compiledFunction({
             id,
@@ -59,10 +59,10 @@ const generateDeposit = async ({
             interest,
             totalAmount,
             typeDeposit,
-            deposit,
+            deposit
         });
 
-        const options = { format: 'A4' };
+        const options = { format: "A4" };
         const pdfBuffer = await html_to_pdf.generatePdf({ content }, options);
         return pdfBuffer;
     } catch (e) {
@@ -76,5 +76,5 @@ module.exports = {
     covertPlainObject,
     formatMoney,
     randomCharacters,
-    generateDeposit,
+    generateDeposit
 };
