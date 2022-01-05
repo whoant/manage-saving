4473560e8e01a98b9f4eda39a57154bf51bcc72f
Vo Van Hoang Tuan <vovanhoangtuan4.2@gmail.com>
2022-01-05 23:25:07 +0700

    Update filter interest

--- src/controllers/manager.controller.js
+++ src/controllers/manager.controller.js
@@ -1,30 +1,59 @@
-const { Period, Interest } = require('../models/index');
-const { formatDate, hash256 } = require('../utils');
+const { Period, Interest } = require("../models/index");
+const { formatDate, hash256, covertPlainObject } = require("../utils");
 
 module.exports.index = async (req, res, next) => {
     const { user } = res.locals;
-    res.render('manager/index', user);
+    res.render("manager/index", user);
 };
 
 module.exports.indexInterest = async (req, res, next) => {
     try {
-        const periods = await Period.findAll({ order: [['month', 'DESC']] });
-        const interests = await Interest.findAll({
-            include: 'Period',
-            raw: true,
-            nest: true,
-            order: [['createdAt', 'DESC']],
-        });
+        let { type } = req.query;
 
-        const interestsRender = interests.map((interest) => {
-            const { createdAt } = interest;
-            return { ...interest, created: formatDate(createdAt, 'VN') };
-        });
-        const messages = await req.consumeFlash('info');
-        res.render('manager/interest', {
+        const periods = await getListPeriods();
+
+        let interests = [];
+        let interestsRender = [];
+        if (type === "all") {
+            interests = await Interest.findAll({
+                include: "Period",
+                raw: true,
+                nest: true,
+                order: [["createdAt", "DESC"]]
+            });
+
+            interestsRender = interests.map((interest) => {
+                const { createdAt } = interest;
+                interest.periodName = interest.Period.name;
+                return { ...interest, created: formatDate(createdAt, "VN") };
+            });
+        } else {
+
+            const listPeriodsRender = [];
+            covertPlainObject(periods).forEach((period) => {
+                if (period.Interests.length !== 0) {
+                    listPeriodsRender.push({ ...period, Interests: period.Interests[0] });
+                }
+            });
+            interestsRender = listPeriodsRender.map(period => {
+                const { Interests } = period;
+                const { name, factor, createdAt } = Interests;
+                return {
+                    name: name,
+                    factor: factor,
+                    periodName: period.name,
+                    created: formatDate(createdAt, "VN")
+                };
+            });
+        }
+
+        const messages = await req.consumeFlash("info");
+        
+        res.render("manager/interest", {
             periods,
             interests: interestsRender,
             messages,
+            type
         });
     } catch (e) {
         next(e);
@@ -35,8 +64,8 @@ module.exports.createInterest = async (req, res, next) => {
     const { name, factor, periodId } = req.body;
     try {
         await Interest.create({ name, factor, periodId });
-        await req.flash('info', 'Thêm lãi suất mới thành công !');
-        res.redirect('back');
+        await req.flash("info", "Thêm lãi suất mới thành công !");
+        res.redirect("back");
     } catch (e) {
         next(e);
     }
@@ -44,13 +73,13 @@ module.exports.createInterest = async (req, res, next) => {
 
 module.exports.indexPeriods = async (req, res, next) => {
     try {
-        const periods = await Period.findAll({ order: [['month', 'DESC']] });
+        const periods = await Period.findAll({ order: [["month", "DESC"]] });
         const [messages, errors] = await Promise.all([
-            req.consumeFlash('info'),
-            req.consumeFlash('error'),
+            req.consumeFlash("info"),
+            req.consumeFlash("error")
         ]);
 
-        res.render('manager/periods', { periods, messages, errors });
+        res.render("manager/periods", { periods, messages, errors });
     } catch (e) {
         next(e);
     }
@@ -62,33 +91,33 @@ module.exports.createPeriods = async (req, res, next) => {
     try {
         const getMonth = await Period.findOne({
             where: {
-                month,
-            },
+                month
+            }
         });
 
         if (getMonth) {
-            throw new Error('Số tháng này đã tồn tại trên hệ thống !');
+            throw new Error("Số tháng này đã tồn tại trên hệ thống !");
         }
 
         await Period.create({ name, month });
-        await req.flash('info', 'Thêm kì hạn thành công !');
+        await req.flash("info", "Thêm kì hạn thành công !");
     } catch (e) {
-        await req.flash('error', e.message);
+        await req.flash("error", e.message);
         next(e);
     }
-    res.redirect('back');
+    res.redirect("back");
 };
 
 module.exports.getDetailPassword = async (req, res, next) => {
     const { user } = res.locals;
     const [messages, errors] = await Promise.all([
-        req.consumeFlash('info'),
-        req.consumeFlash('error'),
+        req.consumeFlash("info"),
+        req.consumeFlash("error")
     ]);
-    res.render('manager/info', {
+    res.render("manager/info", {
         name: user.name,
         messages,
-        errors,
+        errors
     });
 };
 
@@ -98,20 +127,31 @@ module.exports.updatePassword = async (req, res, next) => {
     try {
         const { user } = res.locals;
         if (newPassword !== repeatPassword) {
-            throw new Error('Hai mật khẩu không khớp nhau');
+            throw new Error("Hai mật khẩu không khớp nhau");
         }
 
         if (user.password !== hash256(oldPassword)) {
-            throw new Error('Mật khẩu cũ không khớp !');
+            throw new Error("Mật khẩu cũ không khớp !");
         }
 
         user.password = hash256(newPassword);
         await user.save();
 
-        await req.flash('info', 'Cập nhập mật khẩu thành công !');
+        await req.flash("info", "Cập nhập mật khẩu thành công !");
     } catch (e) {
-        await req.flash('error', e.message);
+        await req.flash("error", e.message);
         next(e);
     }
-    res.redirect('/manager/info');
+    res.redirect("/manager/info");
 };
+
+
+function getListPeriods() {
+    return Period.findAll({
+        include: Interest,
+        order: [
+            ["month", "ASC"],
+            [Interest, "createdAt", "DESC"]
+        ]
+    });
+}
